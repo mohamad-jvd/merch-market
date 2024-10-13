91105f48ca7a6acf605587133f09413f348c56db
mohammad javad <javat200@gmail.com>
2024-10-13 17:52:43 +0330

    amaze

--- merch_market/settings.py
+++ merch_market/settings.py
@@ -85,6 +85,12 @@ DATABASES = {
         'NAME': '/home/m0hamadjvd/merch-market/db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
